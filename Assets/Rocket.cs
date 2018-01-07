dc566ac2c9be16a383940dee620a79c06d2adbb5
Alexandre Le Jeune <lejeunealex@gmail.com>
2018-01-07 20:02:59 +0100

    Added audio for thrust + minor refactor

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
 
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
 	// Use this for initialization
 	void Start () {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -19,17 +21,35 @@ public class Rocket : MonoBehaviour {
 
     private void ProcessInput()
     {
+        ManageThrust();
+        ManageRotation();
+    }
+
+    private void ManageRotation()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+
+    private void ManageThrust()
+    {
         if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        else
         {
-            print("Rotating left");
-        }
-        else if(Input.GetKey(KeyCode.RightArrow))
-        {
-            print("Rotating right");
+            audioSource.Stop();
         }
     }
 }
